9a7c304be2c715b63601f71761ab907aa9084817
OnurKucukoz <onur@kucukoz.com>
2024-07-22 12:40:38 +0300

    Classes have been created for the beginning

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    
+    [SerializeField] private Rigidbody playerRb;
+    [SerializeField] private float speed = 5.0f;
+    private float sensitivityX = 5.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+    }
+
+    //ABSTRACTION
+    void PlayerMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(horizontalInput * Time.deltaTime * Vector3.right * speed);
+        transform.Translate(verticalInput * Time.deltaTime * Vector3.forward * speed);
+
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+    }
+}
