d55e809c2d48dd3122e2cfc3513f7c8e2a71f489
OnurKucukoz <onur@kucukoz.com>
2024-07-27 21:17:20 +0300

    Virtual camera added
    Virtual camera added  to lock on target

---
+++ Assets/Scripts/FocusOnEnemy.cs
@@ -0,0 +1,11 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FocusOnEnemy : MonoBehaviour
+
+{
+   
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,7 +25,6 @@ public class Player : MonoBehaviour
     {
         PlayerMovement();
         transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up);
-
     }
 
     //ABSTRACTION
