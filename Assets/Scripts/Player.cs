903a0b3cbe8e4ee96471ea59d6310e9a089902c7
OnurKucukoz <onur@kucukoz.com>
2024-07-30 14:59:53 +0300

    Bug fixes on lock enemy mechanism

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,28 +13,33 @@ public class Player : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
 
-    
-    
+
+    public float rotationSpeed = 10f;
+    public bool isLocked = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isLocked = false;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        PlayerMovement();
-        //transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
+
+        SwitchLockState();
+            
+        
     }
 
     //ABSTRACTION
-    void PlayerMovement()
+    void FreelookPlayerMovement()
     {
-        transform.rotation = Camera.main.transform.rotation;
+        //transform.rotation = Camera.main.transform.rotation;
 
+
+        
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
@@ -52,12 +57,71 @@ public class Player : MonoBehaviour
         Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeVerticalInput;
         transform.Translate(cameraRelativeMovement, Space.World);
 
+        Vector3 direction = (forward * verticalInput + right * horizontalInput).normalized;
+
+
+
+        Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+
 
         //transform.Translate(horizontalInput * Time.deltaTime * Vector3.right * speed);
-        //transform.Translate(verticalInput * Time.deltaTime * Vector3.forward * speed);
+       // transform.Translate(verticalInput * Time.deltaTime * Vector3.forward * speed);
 
+       // transform.position += direction * speed * Time.deltaTime;
+    }
+    public Transform target;
+    void LockedOnPlayerMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+        forward = forward.normalized;
+        right = right.normalized;
+
+        Vector3 forwardRelativeVerticalInput = verticalInput * forward * Time.deltaTime * speed;
+        Vector3 rightRelativeVerticalInput = horizontalInput * right * Time.deltaTime * speed;
+
+        Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeVerticalInput;
+        transform.Translate(cameraRelativeMovement, Space.World);
+
+        // Vector3 direction = (forward * verticalInput + right * horizontalInput).normalized;
+
+        Vector3 directionToTarget = target.position - transform.position;
+        directionToTarget.y = 0;
+
+        if (directionToTarget.magnitude > 0.1f) // Avoid rotating if too close
+        {
+            Quaternion toRotation = Quaternion.LookRotation(directionToTarget);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        }
+
+    void SwitchLockState()
+    {
+        // Check for "F" key press to toggle lock-on state
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isLocked = !isLocked;
+        }
+
+        // Call the appropriate movement method based on isLocked state
+        if (isLocked)
+        {
+            LockedOnPlayerMovement();
+        }
+        else
+        {
+            FreelookPlayerMovement();
+        }
 
     }
 
-    
+
 }
