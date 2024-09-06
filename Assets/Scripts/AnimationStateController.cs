1e995ba0735763464e07520b287664cbd6b6b7aa
OnurKucukoz <onur@kucukoz.com>
2024-09-06 15:29:25 +0300

    Player animations finished.
    The player can dodge, lock on, strafe, run, walk, perform light attacks, and perform heavy attacks without any bugs.

--- Assets/Scripts/AnimationStateController.cs
+++ Assets/Scripts/AnimationStateController.cs
@@ -23,8 +23,11 @@ public class AnimationStateController : MonoBehaviour
 
     int isDodgingHash;
 
-    public bool isDodging = false;
+    int isAttackingLight;
+    int isAttackingHeavy;
 
+    public bool isDodging;
+    public float cooldownTimer = 0;
 
     void Start()
     {
@@ -47,9 +50,12 @@ public class AnimationStateController : MonoBehaviour
         isLockedOnRightStrafingHash = Animator.StringToHash("isLockedOnRightStrafing");
 
         isDodgingHash = Animator.StringToHash("isDodging");
+
+        isAttackingLight = Animator.StringToHash("isAttackingLight");
+        isAttackingHeavy = Animator.StringToHash("isAttackingHeavy");
     }
 
-    
+
     void Update()
     {
         isFreeLookActive = cameraSwitcher.isFreeLookActive;
@@ -72,6 +78,336 @@ public class AnimationStateController : MonoBehaviour
         bool rightPressed = Input.GetKey("d");
         bool backPressed = Input.GetKey("s");
         bool spacePressed = Input.GetKeyDown("space");
+
+        bool leftMouseClicked = Input.GetMouseButtonDown(0);
+        bool rightMouseClicked = Input.GetMouseButton(1);
+
+        bool isLightAttacking = false;
+        bool isHeavyAttacking = false;
+
+
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+
+        if (isFreeLookActive && leftMouseClicked && !isLightAttacking)
+        {
+
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.30f;
+                FreeLookLightAttack();
+            }
+        }
+
+        if (!isFreeLookActive && leftMouseClicked && !isLightAttacking)
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.30f;
+                LockedOnLightAttack();
+            }
+        }
+
+        if (isFreeLookActive && rightMouseClicked && !isHeavyAttacking)
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.44f;
+                FreeLookHeavyAttack();
+            }
+        }
+
+
+        if (!isFreeLookActive && rightMouseClicked && !isHeavyAttacking)
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.44f;
+                LockedOnHeavyAttack();
+            }
+        }
+
+        if (isFreeLookActive && spacePressed && !isDodging)
+        {
+            if(cooldownTimer <= 0)
+            {
+                cooldownTimer =1.37f;
+                FreeLookDodge();
+            }
+        }
+
+        if (!isFreeLookActive && spacePressed && !isDodging && forwardPressed && !rightPressed && !leftPressed)
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.37f;
+                LockedOnForwardDodge();
+            }
+        }
+
+
+        if (!isFreeLookActive && spacePressed && !isDodging && leftPressed && !backPressed && !forwardPressed)
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.37f;
+                LockedOnLeftDodge();
+            }
+        }
+
+        if (!isFreeLookActive && spacePressed && !isDodging && rightPressed && !backPressed && !forwardPressed)
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.37f;
+                LockedOnRightDodge();
+            }
+        }
+
+        if (!isFreeLookActive && spacePressed && !isDodging && backPressed && !rightPressed && !leftPressed)
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.37f;
+                LockedOnBackwardDodge();
+            }
+        }
+
+        
+        if (!isFreeLookActive && spacePressed && !isDodging && (backPressed && rightPressed))
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.37f;
+                LockedOnBackwardRightDiagonalRolling();
+            }
+        }
+        
+
+        if (!isFreeLookActive && spacePressed && !isDodging && (backPressed && leftPressed))
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.37f;
+                LockedOnBackwardLeftDiagonalRolling();
+            }
+        }
+
+      
+        if (!isFreeLookActive && spacePressed && !isDodging && (leftPressed && forwardPressed))
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.37f;
+                LockedOnForwardLeftDiagonalRolling();
+            }
+        }
+
+        if (!isFreeLookActive && spacePressed && !isDodging && rightPressed && forwardPressed)
+        {
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 1.37f;
+                LockedOnForwardRightDiagonalRolling();
+            }
+        }
+
+
+        void FreeLookLightAttack()
+        {
+            // start freelook light attack
+            if (isFreeLookActive && leftMouseClicked && !isLightAttacking)
+            {
+                isLightAttacking = true;
+                animator.SetTrigger("isAttackingLight");
+
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("AttackingLight") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isLightAttacking = false;
+            }
+        }
+
+        void LockedOnLightAttack()
+        {
+            // start locked on light attack
+            if (!isFreeLookActive && leftMouseClicked && !isLightAttacking)
+            {
+                isLightAttacking = true;
+                animator.SetTrigger("isAttackingLight");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("AttackingLight") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isLightAttacking = false;
+            }
+        }
+
+        void FreeLookHeavyAttack()
+        {
+            // start freelook heavy attack
+            if (isFreeLookActive && rightMouseClicked && !isHeavyAttacking)
+            {
+                isHeavyAttacking = true;
+                animator.SetTrigger("isAttackingHeavy");
+
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("AttackingHeavy") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isHeavyAttacking = false;
+            }
+        }
+
+        void LockedOnHeavyAttack()
+        {
+            // start locked on heavy attack
+            if (!isFreeLookActive && rightMouseClicked && !isHeavyAttacking)
+            {
+                isHeavyAttacking = true;
+                animator.SetTrigger("isAttackingHeavy");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("AttackingHeavy") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isHeavyAttacking = false;
+            }
+        }
+
+        void FreeLookDodge()
+        {
+            //start freelook rolling 
+            if (isFreeLookActive && spacePressed && !isDodging)
+            {
+                
+                animator.SetTrigger("isDodging");
+                // player.rotationSpeed = 5f;      
+                Debug.Log("is dodging true");
+                isDodging = false;
+                
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Dodging") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                //player.rotationSpeed = 10f;
+
+                Debug.Log("is dodging false");
+                isDodging = true;
+            }
+        }
+
+        void LockedOnForwardDodge()
+        {
+            //start locked on forward rolling 
+            if (!isFreeLookActive && spacePressed && !isDodging && forwardPressed && !rightPressed && !leftPressed)
+            {
+                isDodging = false;
+                animator.SetTrigger("isDodging");
+                Debug.Log("xxx");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Dodging") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                Debug.Log("dddd");
+                isDodging = true;
+            }
+        }
+
+        void LockedOnLeftDodge()
+        {
+            //start locked on left rolling
+            if (!isFreeLookActive && spacePressed && !isDodging && leftPressed && !backPressed && !forwardPressed)
+            {
+                isDodging = false;
+                animator.SetTrigger("isDodgingLeft");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingLeft") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isDodging = false;
+            }
+        }
+
+        void LockedOnRightDodge()
+        {
+            //start locked on right rolling
+            if (!isFreeLookActive && spacePressed && !isDodging && rightPressed && !backPressed && !forwardPressed)
+            {
+                isDodging = false;
+                animator.SetTrigger("isDodgingRight");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingRight") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isDodging = false;
+            }
+        }
+
+        void LockedOnBackwardDodge()
+        {
+            //start locked on backward rolling
+            if (!isFreeLookActive && spacePressed && !isDodging && backPressed && !rightPressed && !leftPressed)
+            {
+                isDodging = false;
+                animator.SetTrigger("isDodgingBackward");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingBackward") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isDodging = false;
+            }
+        }
+
+        void LockedOnBackwardRightDiagonalRolling()
+        {
+            //start locked on backward right diagonal rolling
+            if (!isFreeLookActive && spacePressed && !isDodging && (backPressed && rightPressed))
+            {
+                isDodging = false;
+                animator.SetTrigger("isDodgingBackwardRightDiagonal");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingBackwardRightDiagonal") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isDodging = false;
+            }
+        }
+
+        void LockedOnBackwardLeftDiagonalRolling()
+        {
+            //start locked on backward left diagonal rolling
+            if (!isFreeLookActive && spacePressed && !isDodging && (backPressed && leftPressed))
+            {
+                isDodging = false;
+                animator.SetTrigger("isDodgingBackwardLeftDiagonal");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingBackwardLeftDiagonal") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isDodging = false;
+            }
+        }
+
+        void LockedOnForwardLeftDiagonalRolling()
+        {
+            //start locked on left forward diagonal rolling
+            if (!isFreeLookActive && spacePressed && !isDodging && (leftPressed && forwardPressed))
+            {
+                isDodging = false;
+                animator.SetTrigger("isDodgingLeftDiagonal");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingLeftDiagonal") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isDodging = false;
+            }
+        }
+
+        void LockedOnForwardRightDiagonalRolling()
+        {
+            //start locked on right forward diagonal rolling
+            if (!isFreeLookActive && spacePressed && !isDodging && rightPressed && forwardPressed)
+            {
+                isDodging = false;
+                animator.SetTrigger("isDodgingRightDiagonal");
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingRightDiagonal") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                isDodging = false;
+            }
+        }
+
 
         //start freelook walking
         if (isFreeLookActive && !spacePressed && !isWalking && (forwardPressed || rightPressed || leftPressed || backPressed))
@@ -96,106 +432,6 @@ public class AnimationStateController : MonoBehaviour
             animator.SetBool(isRunningHash, false);
         }
 
-        //start freelook rolling 
-        if (isFreeLookActive && spacePressed && !isDodging)
-        {
-            isDodging = true;
-           // player.rotationSpeed = 5f;      
-            animator.SetTrigger("isDodging");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Dodging") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            //player.rotationSpeed = 10f;
-            isDodging = false;
-        }
-
-        //start locked on forward rolling 
-        if (!isFreeLookActive && spacePressed && !isDodging && forwardPressed && !rightPressed && !leftPressed)
-        {
-            isDodging = true;
-            animator.SetTrigger("isDodging");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Dodging") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isDodging = false;
-        }
-
-        //start locked on right rolling
-        if (!isFreeLookActive && spacePressed && !isDodging && rightPressed &&!backPressed && !forwardPressed)
-        {
-            isDodging = true;
-            animator.SetTrigger("isDodgingRight");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingRight") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isDodging = false;
-        }
-
-        //start locked on left rolling
-        if (!isFreeLookActive && spacePressed && !isDodging && leftPressed && !backPressed && !forwardPressed)
-        {
-            isDodging = true;
-            animator.SetTrigger("isDodgingLeft");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingLeft") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isDodging = false;
-        }
-
-        //start locked on backward rolling
-        if (!isFreeLookActive && spacePressed && !isDodging && backPressed && !rightPressed && !leftPressed)
-        {
-            isDodging = true;
-            animator.SetTrigger("isDodgingBackward");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingBackward") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isDodging = false; 
-        }
-
-        //start locked on backward right diagonal rolling
-        if (!isFreeLookActive && spacePressed && !isDodging && (backPressed && rightPressed))
-        {
-            isDodging = true;
-            animator.SetTrigger("isDodgingBackwardRightDiagonal");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingBackwardRightDiagonal") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isDodging = false; 
-        }
-
-        //start locked on backward left diagonal rolling
-        if (!isFreeLookActive && spacePressed && !isDodging && (backPressed && leftPressed))
-        {
-            isDodging = true;
-            animator.SetTrigger("isDodgingBackwardLeftDiagonal");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingBackwardLeftDiagonal") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isDodging = false; 
-        }
-
-        //start locked on left forward diagonal rolling
-        if (!isFreeLookActive && spacePressed && !isDodging && (leftPressed && forwardPressed))
-        {
-            isDodging = true;
-            animator.SetTrigger("isDodgingLeftDiagonal");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingLeftDiagonal") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isDodging = false;
-        }
-
-        //start locked on right forward diagonal rolling
-        if (!isFreeLookActive && spacePressed && !isDodging && rightPressed && forwardPressed)
-        {
-            isDodging = true;
-            animator.SetTrigger("isDodgingRightDiagonal");
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("DodgingRightDiagonal") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isDodging = false;
-        }
 
         //start locked on walk forward
         if (!isFreeLookActive && !isLockedOnWalkingForward && forwardPressed)
@@ -248,7 +484,7 @@ public class AnimationStateController : MonoBehaviour
         }
 
 
-        //start locked on forward running -onur sana bir not gecmisteki onurdan. startlarda hepsi && li olacak. Stoplarda isfreelook||(....&&!...)
+        //start locked on forward running
         if (!isFreeLookActive && !isLockedOnRunningForward && forwardPressed && shiftPressed)
         {
             animator.SetBool(isLockedOnRunningForwardHash, true);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] AnimationCurve dodgeCurve;
+    
     bool isDodging;
     float dodgeTimer;
 
@@ -29,20 +29,123 @@ public class Player : MonoBehaviour
         isLocked = false;
         animator = GetComponent<Animator>();
 
-        Keyframe dodge_lastFrame = dodgeCurve[dodgeCurve.length - 1];
-        dodgeTimer = dodge_lastFrame.time;
+       
     }
 
-    // Update is called once per frame
+    
     void LateUpdate()
     {
         SwitchLockState();
     }
 
+    bool isLightClicked = false;
+    bool isHeavyClicked = false;
+    bool isSpacePressed = false;
 
     //ABSTRACTION
     void FreelookPlayerMovement()
     {
+        if (Input.GetMouseButtonDown(0) && !isLightClicked && !isHeavyClicked)
+        {
+            StartCoroutine(StopPlayerMovementLight());
+        }
+
+        if (Input.GetMouseButtonDown(1) && !isHeavyClicked && !isLightClicked)
+        {
+            StartCoroutine(StopPlayerMovementHeavy());
+        }
+
+       
+        horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 forward = Camera.main.transform.forward;
+            Vector3 right = Camera.main.transform.right;
+
+            forward.y = 0;
+            right.y = 0;
+            forward = forward.normalized;
+            right = right.normalized;
+
+            Vector3 forwardRelativeVerticalInput = verticalInput * forward * Time.deltaTime * speed;
+            Vector3 rightRelativeVerticalInput = horizontalInput * right * Time.deltaTime * speed;
+
+            Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeVerticalInput;
+            transform.Translate(cameraRelativeMovement, Space.World);
+
+            direction = (forward * verticalInput + right * horizontalInput).normalized;
+
+            if (direction != Vector3.zero)
+            {
+                //animator.SetBool("isWalking", true);
+                Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+                transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            } 
+    }
+
+    
+
+    IEnumerator StopPlayerMovementLight()
+    {
+
+        isLightClicked = true;
+        speed = 0;
+        rotationSpeed = 1.20f;
+        yield return new WaitForSeconds(1.44f);
+
+        speed = 5;
+        rotationSpeed = 10f;
+        isLightClicked = false;
+         
+    }
+
+
+
+    IEnumerator StopPlayerMovementHeavy()
+    {
+        isHeavyClicked = true;
+        speed = 0;
+        rotationSpeed = 1.20f;
+        yield return new WaitForSeconds(1.44f);
+
+        speed = 5;
+        rotationSpeed = 10f;
+        isHeavyClicked = false;
+
+    }
+
+
+    IEnumerator StopPlayerMovementDodge()
+    {
+
+        isSpacePressed = true;
+        speed = 1;
+        rotationSpeed = 1.20f;
+        yield return new WaitForSeconds(1.20f);
+
+        speed = 5;
+        rotationSpeed = 10f;
+        isSpacePressed = false;
+
+    }
+
+    void LockedOnPlayerMovement()
+    {
+        if (Input.GetMouseButtonDown(0) && !isLightClicked && !isHeavyClicked)
+        {
+            StartCoroutine(StopPlayerMovementLight());
+        }
+
+        if (Input.GetMouseButtonDown(1) && !isHeavyClicked)
+        {
+            StartCoroutine(StopPlayerMovementHeavy());
+        }
+
+        if (Input.GetKeyDown("space") )
+        {
+            StartCoroutine(StopPlayerMovementDodge());
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
@@ -57,41 +160,11 @@ public class Player : MonoBehaviour
         Vector3 forwardRelativeVerticalInput = verticalInput * forward * Time.deltaTime * speed;
         Vector3 rightRelativeVerticalInput = horizontalInput * right * Time.deltaTime * speed;
 
-        Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeVerticalInput;
-        transform.Translate(cameraRelativeMovement, Space.World);
-        
-        direction = (forward * verticalInput + right * horizontalInput).normalized;
-
-        if (direction != Vector3.zero)
+       /* if (rightRelativeVerticalInput.magnitude != 0)
         {
-            animator.SetBool("isWalking", true);
-            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
-            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            verticalSpeed = 0f;
         }
-    }
-
-
-    void LockedOnPlayerMovement()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 forward = Camera.main.transform.forward;
-        Vector3 right = Camera.main.transform.right;
-
-        forward.y = 0;
-        right.y = 0;
-        forward = forward.normalized;
-        right = right.normalized;
-
-        Vector3 forwardRelativeVerticalInput = verticalInput * forward * Time.deltaTime * verticalSpeed;
-        Vector3 rightRelativeVerticalInput = horizontalInput * right * Time.deltaTime * speed;
-
-        if (rightRelativeVerticalInput.magnitude != 0)
-        {
-            verticalSpeed = 3f;
-        }
-        else { verticalSpeed = 5f; }
+        else { verticalSpeed = 0f; }*/
 
         Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeVerticalInput;
         transform.Translate(cameraRelativeMovement, Space.World);
@@ -99,7 +172,7 @@ public class Player : MonoBehaviour
         Vector3 directionToTarget = target.position - transform.position;
         directionToTarget.y = 0;
 
-        if (directionToTarget.magnitude > 0.1f) // Avoid rotating if too close
+        if (directionToTarget.magnitude > 1f) // Avoid rotating if too close
         {
             Quaternion toRotation = Quaternion.LookRotation(directionToTarget);
             transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
